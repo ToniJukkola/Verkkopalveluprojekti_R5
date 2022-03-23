033453191683328fdfd62542e1e6ebbf6b9c80ca
n1vial00 <n1vial00@students.oamk.fi>
2022-03-23 11:01:01 +0200

    Aloitettu sivupohjan kääntö Reactiin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
+
+import Footer from './comp/Footer';
+import Header from './comp/Header';
+import About from "./comp/pages/About";
+
+import Home from './comp/pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/comp/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+export default function Footer() {
+    return(
+        <footer className="container-fluid">
+        <div>
+            <div className="footer-title">Kukkakauppa Oy</div>
+            <p>Katunimi 13,<br />
+            00000 Kaupunki</p>
+        </div>
+        <div>
+            <div className="footer-title">Sivukartta</div>
+            <ul className="list-unstyled">
+                <a href="https://google.com"><li>Linkki</li></a>
+                <a href="https://google.com"><li>Linkki</li></a>
+                <a href="https://google.com"><li>Linkki</li></a>
+            </ul>
+        </div>
+        <div>
+            <div className="footer-title">Seuraa meitä sosiaalisessa mediassa!</div>
+            <div className="footer-social-media">
+                <a href="https://google.com"><i className="bi bi-facebook"></i></a>
+                <a href="https://google.com"><i className="bi bi-instagram"></i></a>
+                <a href="https://google.com"><i className="bi bi-youtube"></i></a>
+            </div>
+        </div>
+        </footer>
+    );
+}
