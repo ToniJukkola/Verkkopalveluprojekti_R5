d9af9cb25ee1c9c6ef8d6e020110a8104c2c8f1b
n1vial00 <n1vial00@students.oamk.fi>
2022-03-28 14:54:15 +0300

    Kuvien linkitystä tutkittu karuselliä varten.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,16 @@ import React from "react";
 export default function Home() {
     return (
         <>
-            <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+            <div id="carouselExampleSlidesOnly" className="carousel slide interval" data-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img className="d-block w-100" src="..." alt="First slide" />
+                        <img className="d-block w-100" src={require(".././images/tuotenro_1.jpg")} alt="First slide" />
                     </div>
                     <div className="carousel-item">
-                        <img className="d-block w-100" src="..." alt="Second slide" />
+                        <img className="d-block w-100" src={require(".././images/tuotenro_2.jpg")} alt="Second slide" />
                     </div>
                     <div className="carousel-item">
-                        <img className="d-block w-100" src="..." alt="Third slide" />
+                        <img className="d-block w-100" src={require(".././images/tuotenro_3.jpg")} alt="Third slide" />
                     </div>
                 </div>
             </div>
