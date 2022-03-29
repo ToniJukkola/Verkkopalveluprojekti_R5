9900ecc0a345914e3918263cd0e46ed8cf576aab
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-03-29 15:54:46 +0300

    Tuotelistaukselle lisätty tyylit

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -7,29 +7,44 @@ import {
 } from "react-router-dom";
 
 export default function Category(props) {
-    const [productList, setProductList] = useState([]);
-    let params = useParams();
-    
-    useEffect(() => {
+  const [productList, setProductList] = useState([]);
+  let params = useParams();
+
+  useEffect(() => {
     console.log(params);
-    if(params.trnro > 0){
+    if (params.trnro > 0) {
       axios.get(URL + "get_products.php/" + params.trnro
-    )
-      .then((response) => {
-        setProductList(response.data)
-      }).catch(error => {
-        alert(error);
-      });}
-    },[params]);
+      )
+        .then((response) => {
+          setProductList(response.data)
+        }).catch(error => {
+          alert(error);
+        });
+    }
+  }, [params]);
 
-    return (
-        <>
+  return (
+    <>
+      <div className="product-list-wrapper">
+        {/* TUOTEKORTTIEN MAPPAUS ALKAA */}
+        {productList.products?.map(e => (
+          <div className="card" key={e.tuotenro}>
+            <img src={require(".././images/tuotenro_1.jpg")} className="card-img-top" alt="" />
+            <div className="card-body">
+              <div>
+                <h5 className="card-title">{e.tuotenimi}</h5>
+                <h6 className="card-title">{e.hinta}</h6>
+              </div>
+              <p className="card-text">{e.tuotekuvaus}
+                <Link to={"/tuote/" + e.tuotenro}>Lue lisää</Link>
+              </p>
 
-            <ul>
-            {productList.products?.map(e => (
-                <li key={e.tuotenro}> <Link to={"/tuote/" + e.tuotenro}> {e.tuotenimi} {e.tuotekuvaus}</Link> </li>
-            ))}
-            </ul>
-        </>
-    );
+              <a href="#" className="btn"><i className="bi bi-bag-fill"></i> Lisää ostoskoriin</a>
+            </div>
+          </div>
+        ))}
+        {/* TUOTEKORTTIEN MAPPAUS PÄÄTTYY */}
+      </div>
+    </>
+  );
 }
