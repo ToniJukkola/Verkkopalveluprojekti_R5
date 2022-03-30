e9f90844d2341583e7ffcc009e2d08bc5719456f
ToniJukkola <n1juto00@students.oamk.fi>
2022-03-30 20:08:10 +0300

    Lisäys lomake

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -11,25 +11,26 @@ export default function Add() {
                <button type="button" className="btn btn-outline-dark">Poista tuote</button>
             </ul><br></br>
                 <ul>
-                    <label>Uusi tuote:</label>
+                    <h4>Uusi tuote:</h4>
                 </ul>
-                    <ul>
-                        <input placeholder="Nimi"></input>
-                    </ul>
-                        <ul>
-                            <input type="number" step="0.01" placeholder="Hinta"></input>
-                        </ul>
-                            <ul>
-                                <input type="text" placeholder="Kuvaus"></input>
-                            </ul>
-                                <ul>
-                                    <input placeholder="Hoito-ohje"></input>
-                                </ul>
-                                    <ul>
-                                        <input placeholder="Tieteellinen nimi"></input>
-                                    </ul>
-                        
-                
+                <ul>
+                    <textarea placeholder="Nimi"></textarea>
+                </ul>
+                <ul>
+                    <textarea type="textarea" placeholder="Kuvaus"></textarea>
+                </ul>
+                <ul>
+                    <textarea placeholder="Hoito-ohje"></textarea>
+                </ul>
+                <ul>
+                    <textarea placeholder="Tieteellinen nimi"></textarea>
+                </ul>
+                <ul>
+                    <input type="number" step="0.01" placeholder="Hinta"></input>
+                </ul>
+                <ul>
+                    <button type="button" className="btn btn-outline-dark">Lisää uusi tuote</button>
+                </ul>
         </>
     );
 }
