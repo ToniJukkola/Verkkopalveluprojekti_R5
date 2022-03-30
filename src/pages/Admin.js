9c83bdd653d80093cc340bf2a45fa67bd6e25e89
ToniJukkola <n1juto00@students.oamk.fi>
2022-03-30 13:29:43 +0300

    Admin sivu alussa

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function Admin() {
     return (
         <>
-            Nyt sitä hyvää
+            <h1>Tervetuloa ylläpitäjä!</h1>
+            <ul> 
+               <button type="button" class="btn btn-dark">Lisää tuote</button>
+            </ul>
+           <ul> 
+               <button type="button" class="btn btn-dark">Poista tuote</button>
+            </ul>
         </>
     );
 }
