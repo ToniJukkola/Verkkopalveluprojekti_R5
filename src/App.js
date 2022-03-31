9034199c3d27043daecb7b39fc9543322d83beef
oonakm <oona.maattala@hotmail.com>
2022-03-31 12:21:58 +0300

    ostoskoriyritys

--- src/App.js
+++ src/App.js
@@ -49,3 +49,32 @@ function App() {
 }
 
 export default App;
+
+export default function Order({cart}) {
+  let sum = 0;
+
+  return (
+    <div>
+      <h3 className="header">Ostoskorin tuotteet</h3>
+      <table className="table">
+        <tbody>
+          {cart.map(product => {
+            sum+=parseFloat(product.price);
+            return (
+              <tr key={uuid()}>
+                <td>{product.name}</td>
+                <td>{product.name} €</td>
+                <td></td>
+              </tr>
+            )
+          })}
+          <tr key={uuid()}>
+            <td></td>
+            <td>{sum.toFixed(2)} €</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
