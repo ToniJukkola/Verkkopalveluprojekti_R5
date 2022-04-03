6d65addcb097e0a2bd2d29b20efbfaa580ac671d
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-04-03 14:40:11 +0300

    Nimetty uudelleen Delete > DeleteProduct

--- src/pages/DeleteProduct.js
+++ src/pages/DeleteProduct.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 
-export default function Delete({ url }) {
+export default function DeleteProduct({ url }) {
     const [products, setProducts] = useState([]);
     useEffect(() => {
         axios.get(url + "products/get_products-all.php")
@@ -13,21 +13,14 @@ export default function Delete({ url }) {
                 alert(error.response === undefined ? error : error.response.data.error);
             })
     }, [])
-    
-    
+
+
     return (
         <main className="container">
-             <h1>Tuotteiden poistaminen</h1>
-             <ul> 
-               <Link className="btn btn-outline-dark" to={"/admin/addproduct/"}>Lisää tuote</Link>
-            </ul>
-                <ul> 
-                <Link className="btn btn-outline-dark" to={"/admin/delete/"}>Poista tuote</Link>
-                 </ul>
-                 <ul> 
-                <Link className="btn btn-outline-dark" to={"/admin/addcategory/"}>Lisää tuoteryhmä</Link>
-                </ul>
-            <br />
+            <h1>Tuotteiden poistaminen</h1>
+            <Link className="p-3" to={"/admin/"}>&larr; Takaisin ylläpidon etusivulle</Link>
+
+            <br /><br />
             <form>
                 <ul><h4>Valitse tuotteet jotka haluat poistaa valikoimasta</h4></ul>
             </form>
@@ -41,7 +34,7 @@ export default function Delete({ url }) {
                 ))}
             </div>
             <ul>
-            <button type="button" className="btn btn-outline-dark">Poista tuote</button>
+                <button type="button" className="btn btn-outline-dark">Poista tuote</button>
             </ul>
         </main>
     );
