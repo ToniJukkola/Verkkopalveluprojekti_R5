d7bb9c6a004eeb86373f4c1806d7dcf3ca1ab378
ToniJukkola <n1juto00@students.oamk.fi>
2022-04-05 20:38:36 +0300

    AddProductia rakenneltu (ei toimi vielä)

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -5,6 +5,12 @@ import { Link } from 'react-router-dom'
 
 
 export default function AddProduct({ url }) {
+    const [name, setName] = useState([]);
+    const [desc, setDesc] = useState([]);
+    const [instruction, setInstruction] = useState([]);
+    const [othername, setOthername] = useState([]);
+    const [price, setPrice] = useState([]);
+    const [category, selectCategory] = useState([]);
     const [categories, setCategories] = useState([]);
     const [selectedFile, setSelectedFile] = useState(null);
 
@@ -18,6 +24,15 @@ export default function AddProduct({ url }) {
     }, []);
 
     function addNewProduct(e) {
+        e.preventDefault();
+        const json = JSON.stringify({ tuotenimi: name },{ tuotekuvaus: desc },{ ohje: instruction },{ tieteellinen_nimi: othername },{ hinta: price },{ trnimi: category });
+        axios.post(url + 'admin/add_product.php', json, {
+        headers: {
+        'Content-Type': 'application/json'
+        }
+    })
+        
+
         e.preventDefault();
         // Tää koodi tulee sit Tonin tuotekoodin .then sisälle
         let data = new FormData();
@@ -42,39 +57,37 @@ export default function AddProduct({ url }) {
             <h1>Tuotteiden lisääminen</h1>
             <Link className="p-3" to={"/admin/"}>&larr; Takaisin ylläpidon etusivulle</Link>
             <form onSubmit={addNewProduct}>
-            <ul><br></br>
-                <h4>Uusi tuote:</h4>
-            </ul>
-            <div className="mt-5 col-lg-6 col-sm">
-            <ul>
-                <textarea className="form-control" placeholder="Nimi"></textarea>
-            </ul>
-            <ul>
-                <textarea className="form-control" placeholder="Kuvaus"></textarea>
-            </ul>
-            <ul>
-                <textarea className="form-control" placeholder="Hoito-ohje"></textarea>
-            </ul>
-            <ul>
-                <textarea className="form-control" placeholder="Tieteellinen nimi"></textarea>
-            </ul>
-            <ul>
-                <input className="form-control" type="number" step="0.01" placeholder="Hinta"></input>
-            </ul>
-            <ul>
-                <select className='form-control'>
-                    {categories?.map(category => (
-                        <option key={category.trnro}>{category.trnimi}
-                        </option>))};
-                </select>
-            </ul>
-            <ul>
-                <input type="file" className='form-control' name='upload_file' onChange={handleFileSelect} />
-            </ul>
-            <ul>
-                <button type="submit" className="btn btn-outline-dark">Lisää uusi tuote</button>
-            </ul>
-           </div>
+                <div className="mt-5 col-lg-6 col-sm">
+                    <ul>
+                        <h4>Uusi tuote:</h4>
+                        <input className="form-control" placeholder="Nimi" value={name} onChange={e => setName(e.target.value)}></input>
+                    </ul>
+                    <ul>
+                        <input className="form-control" placeholder="Kuvaus" value={desc} onChange={e => setDesc(e.target.value)}></input>
+                    </ul>
+                    <ul>
+                        <input className="form-control" placeholder="Hoito-ohje" value={instruction} onChange={e => setInstruction(e.target.value)}></input>
+                    </ul>
+                    <ul>
+                        <input className="form-control" placeholder="Tieteellinen nimi" value={othername} onChange={e => setOthername(e.target.value)}></input>
+                    </ul>
+                    <ul>
+                        <input className="form-control" type="number" step="0.01" placeholder="Hinta" value={price} onChange={e => setPrice(e.target.value)}></input>
+                    </ul>
+                    <ul>
+                        <select className='form-control' value={category} onChange={e => selectCategory(e.target.value)}>
+                            {categories?.map(category => (
+                                <option key={category.trnro}>{category.trnimi}
+                                </option>))};
+                        </select>
+                    </ul>
+                    <ul>
+                        <input type="file" className='form-control' name='upload_file' onChange={handleFileSelect} />
+                    </ul>
+                    <ul>
+                        <button type="submit" className="btn btn-outline-dark">Lisää uusi tuote</button>
+                    </ul>
+                </div>
            </form> 
         </main>
     );
