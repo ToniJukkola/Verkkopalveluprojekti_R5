a929b8d0fbb329e9fe0f0d27e4e9df67174d0636
Kasperi Vaarala <n1vaka00@students.oamk.fi>
2022-04-05 15:00:03 +0300

    product sivua (kesken)

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import './Product.css'
 
 export default function Product({ url }) {
   const [product, setProduct] = useState([]);
@@ -18,12 +19,25 @@ export default function Product({ url }) {
 
   return (
     <>
-    <h3>{product.tuotenimi}</h3>
-    <h3>{product.tieteellinen_nimi}</h3>
-    {/* Kuva tähän jee */}
+    <h3 className='product_name'>{product.tuotenimi}</h3>
+    <p className='scientific_name'>{product.tieteellinen_nimi}</p>
+    {/* <img src={require(".././images/tuotenro_" + product.tuotenro + ".jpg")}/> */}
     <h4>{product.hinta}</h4>
-    <p>{product.tuotekuvaus}</p>
-    <p>{product.ohje}</p>
+    <div>
+       <button type="button" className='cart_button'>Lisää ostoskoriin</button>
+       <label for="amount">KPL</label>
+                                <select name="amount" id="amount">
+                                  <option value="1">1</option>
+                                  <option value="2">2</option>
+                                  <option value="3">3</option>
+                                  <option value="4">4</option>
+                                  <option value="5">5</option>
+                                </select>
+        </div>
+    <div className='circle'>
+    <p className='product_disc'>{product.tuotekuvaus}</p>
+    <p className='product_care'>{product.ohje}</p>
+    </div>
     </>
   )
 }
