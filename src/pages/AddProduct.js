6944d69e34312e85223b5963a9283423f095a39d
n1vial00 <n1vial00@students.oamk.fi>
2022-04-05 13:38:48 +0300

    Kuvien tallennus toimii!

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 
 
 
-export default function AddCategory({ url }) {
+export default function AddProduct({ url }) {
     const [categories, setCategories] = useState([]);
+    const [selectedFile, setSelectedFile] = useState(null);
 
     useEffect(() => {
         axios.get(url + "products/get_categories.php")
@@ -14,12 +15,33 @@ export default function AddCategory({ url }) {
             }).catch(error => {
                 alert(error.response === undefined ? error : error.response.data.error);
             })
-    }, [])
+    }, []);
+
+    function addNewProduct(e) {
+        e.preventDefault();
+        // Tää koodi tulee sit Tonin tuotekoodin .then sisälle
+        let data = new FormData();
+        data.append('file', selectedFile[0]);
+        console.log(data);
+        
+        axios.post(url + "admin/save_img.php", data)
+        .then((response) => {
+            console.log(response);
+        }).catch(error => {
+            alert(error.response ? error.response.data.error : error);
+        });
+    }
+
+    const handleFileSelect = (event) => {
+        setSelectedFile(event.target.files);
+        console.log(event.target.files);
+    }
 
     return (
         <main className="container">
             <h1>Tuotteiden lisääminen</h1>
             <Link className="p-3" to={"/admin/"}>&larr; Takaisin ylläpidon etusivulle</Link>
+            <form onSubmit={addNewProduct}>
             <ul><br></br>
                 <h4>Uusi tuote:</h4>
             </ul>
@@ -40,16 +62,20 @@ export default function AddCategory({ url }) {
                 <input className="form-control" type="number" step="0.01" placeholder="Hinta"></input>
             </ul>
             <ul>
-                <select>
+                <select className='form-control'>
                     {categories?.map(category => (
                         <option key={category.trnro}>{category.trnimi}
                         </option>))};
                 </select>
             </ul>
             <ul>
-                <button type="button" className="btn btn-outline-dark">Lisää uusi tuote</button>
+                <input type="file" className='form-control' name='upload_file' onChange={handleFileSelect} />
             </ul>
-           </div> 
+            <ul>
+                <button type="submit" className="btn btn-outline-dark">Lisää uusi tuote</button>
+            </ul>
+           </div>
+           </form> 
         </main>
     );
 }
