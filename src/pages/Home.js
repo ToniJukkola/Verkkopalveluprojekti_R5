fb03565849ccee2040857097ce69472c848d3759
n1vial00 <n1vial00@students.oamk.fi>
2022-04-05 15:02:24 +0300

    Karuselli toimii. Tyylit puuttuu.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,22 +19,20 @@ export default function Home({ url }) {
             <Hero />
 
             <main className="container">
-                <div id="carouselControls" className="carousel slide" data-ride="carousel">
+                <div id="carouselExampleSlidesOnly" className="carousel slide carousel-fade" data-bs-ride="carousel">
                     <div className="carousel-inner">
                         {products?.map((product, index) => 
-                            <div className="carousel-item" key={index}>
-                            <img className="d-block w-100" src={"http://localhost/verkkopalveluprojekti_r5_backend/images/tuotenro_" + product.tuotenro + ".jpg"} alt="First slide" />
+
+                        index === 0 ? (
+                            <div className="carousel-item active" key={index}>
+                            <img className="d-block w-100" src={"http://localhost/verkkopalveluprojekti_r5_backend/images/tuotenro_" + product.tuotenro + ".jpg"} alt={product.tuotenimi} />
                             </div>
-                        )}
+                        ) : (
+                            <div className="carousel-item" key={index}>
+                            <img className="d-block w-100" src={"http://localhost/verkkopalveluprojekti_r5_backend/images/tuotenro_" + product.tuotenro + ".jpg"} alt={product.tuotenimi} />
+                            </div>
+                        ))}
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
                 </div>
             </main>
         </>
