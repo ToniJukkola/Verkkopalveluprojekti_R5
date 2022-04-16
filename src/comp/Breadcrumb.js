a4066686a90ed8969fb84afb9edbc9779dab1df9
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-04-16 15:41:14 +0300

    Muutettu murujen logiikkaa. Pieniä ulkonäköparannuksia ja pari kommenttia lisätty.

--- src/comp/Breadcrumb.js
+++ src/comp/Breadcrumb.js
@@ -15,7 +15,7 @@ export default function Breadcrumb({url}) {
       }).catch(error => {
         alert(error.response === undefined ? error : error.response.data.error);
       })
-  }, []);
+  }, [url]);
 
   useEffect(() => {
     axios.get(url + "products/get_products-all.php")
@@ -24,10 +24,19 @@ export default function Breadcrumb({url}) {
         }).catch(error => {
             alert(error.response === undefined ? error : error.response.data.error);
         })
-  }, [])
+  }, [url]);
 
   // Tämä tarkistaa pitääkö polun näyttönimi hakea muualta vai riittääkö sen oma nimi
-  function typeCheck(item,index,pname) {
+  function typeCheck(item, index, pname) {
+    // Lisää tähän listaan nimet joille ei tarvita erillisiä lapsia. Päivitä myös alempaan taulukkoon näytettävä arvo.
+    let singleCrumbs = ["haku", "admin", "ostoskori", "kaikki-tuotteet", "ota-yhteytta"];
+    let singleCrumbsPrettier =  ["Haku", "Admin", "Ostoskori", "Kaikki Tuotteet", "Ota Yhteyttä"];
+
+    for (let index = 0; index < singleCrumbs.length; index++) {
+      if (item === singleCrumbs[index]) {
+        return singleCrumbsPrettier[index];
+      }
+    }
     if(item === "tuotteet") {
       const nIndex = categories.findIndex(e => {
         return e.trnro == pname[1];
@@ -37,7 +46,6 @@ export default function Breadcrumb({url}) {
       const nIndex = products.findIndex(e => {
         return e.tuotenro == pname[1];
       })
-      
       return products[nIndex]?.tuotenimi;
     } else {
       return pname;
@@ -47,7 +55,7 @@ export default function Breadcrumb({url}) {
     const pathnames = location.pathname.split("/").filter(x => x);
     let newOrderPathnames = new Array();
     for (let index = 0; index < pathnames.length; index += 2) {
-      newOrderPathnames[index] = new Array ( pathnames[index],pathnames[index + 1]);
+      newOrderPathnames[index] = new Array(pathnames[index],pathnames[index + 1]);
     }
 
     // isLast tarkistaa onko kyseessä taulukon viimeinen polku. Jos polku on viimeinen, se esitetään tekstimuodossa. 
@@ -59,7 +67,7 @@ export default function Breadcrumb({url}) {
         return isLast ? (
           <span className='breadcrumb-item active' key={index}> {typeCheck(pname[0], index, pname)} </span>
         ) : pname[0] === "tuotteet" || "tuote" ? (
-          <Link className='breadcrumb-item' key={index} to={"/" + pname[0] + "/" + pname[1]}>{typeCheck(pname[0], index, pname)}</Link>
+          <Link className='breadcrumb-item' key={index} to={"/" + pname[0] + "/" + pname[1]}>{typeCheck(pname[0], index , pname)}</Link>
         ) : ("");
       })}
     </ol>
