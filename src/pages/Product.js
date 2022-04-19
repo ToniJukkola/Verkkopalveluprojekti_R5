e81e2d8719ba7c28a8d4835fac1ad732986c2567
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-04-19 22:46:41 +0300

    Lisätty container, fiksailtu vähän asettelua

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -14,35 +14,36 @@ export default function Product({ url, addToCart }) {
       }).catch(error => {
         alert(error.response === undefined ? error : error.response.data.error);
       })
-  }, [params])
+  }, [url, params])
 
   return (
-    <>
-    <div className='grid_container'>
-      <div>
-    <h3 className='product_name'>{product.tuotenimi}</h3>
-    <h4 className='price'>{product.hinta}</h4>
-  
-    <p className='scientific_name'>{product.tieteellinen_nimi}</p>
-    <img src={"http://localhost/verkkopalveluprojekti_r5_backend/images/tuotenro_" + product.tuotenro + ".jpg"} alt={product.tuotenimi} className="img-thumbnail" />
-  </div>
-  <div>
-      <button className="btn btn-accent" onClick={e => addToCart(product)}><i className="bi bi-bag-fill"></i> Lisää ostoskoriin</button>
-      <label htmlFor="amount" className='amount'>KPL</label>
-      <select name="amount" id="amount">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      
-    <div className='circle'>
-    <p className='product_disc'>{product.tuotekuvaus}</p>
-    <p className='product_care'>{product.ohje}</p>
-    </div>
-    </div>
-    </div>
-    </>
+    <main className="container">
+      <div className='grid_container'>
+        <div>
+          <h3 className='product_name'>{product.tuotenimi}</h3>
+          <p className='scientific_name'>{product.tieteellinen_nimi}</p>
+          <img src={"http://localhost/verkkopalveluprojekti_r5_backend/images/tuotenro_" + product.tuotenro + ".jpg"} alt={product.tuotenimi} className="img-thumbnail" />
+          <h4 className='price mt-3'>{product.hinta} €</h4>
+        </div>
+        <div className="text-center">
+          <button className="btn btn-accent" onClick={e => addToCart(product)}><i className="bi bi-bag-fill"></i> Lisää ostoskoriin</button>
+          <label htmlFor="amount" className='amount'>KPL</label>
+          <select name="amount" id="amount">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+
+          <div className='circle'>
+            <div>
+              <p className='product_desc'>{product.tuotekuvaus}</p>
+              <p className='product_care'>{product.ohje}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
