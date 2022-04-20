8ece47930b292b32d275bfb85a6dd28ef632455b
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-04-20 20:45:08 +0300

    Tilausten veto admin-sivulle alustavasti ok

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Link } from 'react-router-dom'
+import Orderlist from "../comp/Orderlist";
 
-export default function Admin() {
+export default function Admin({ url }) {
     return (
         <main className="container">
             <div>
@@ -9,10 +10,14 @@ export default function Admin() {
 
                 <div className="d-grid gap-3 justify-content-start">
                     <Link className="btn btn-outline-dark" to={"/admin/addproduct/"}>Lisää tuote</Link>
-                <Link className="btn btn-outline-dark" to={"/admin/edit/"}>Tuotteiden hallinta</Link>
+                    <Link className="btn btn-outline-dark" to={"/admin/edit/"}>Tuotteiden hallinta</Link>
 
                     <Link className="btn btn-outline-dark" to={"/admin/addcategory/"}>Tuoteryhmien hallinta</Link>
                 </div>
+            </div>
+
+            <div className="mt-5">
+                <Orderlist url={url} />
             </div>
         </main>
     );
---
+++ src/comp/Orderlist.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+export default function Orderlist({ url }) {
+  const [orders, setOrders] = useState([]);
+
+  // Haetaan tilaukset backendistä
+  useEffect(() => {
+    axios.get(url + "admin/get_orders.php")
+      .then((response) => {
+        setOrders(response.data);
+      }).catch(error => {
+        alert(error.response === undefined ? error : error.response.data.error);
+      })
+  }, [url])
+
+  return (
+    <>
+      <h2>Asiakkaiden tekemät tilaukset</h2>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Asiakas</th>
+            <th>Tilattu</th>
+            <th className="text-end">Tuotteet</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Tilausten mappaus alkaa */}
+          {orders.map(order => (
+            <tr key={order.tilausnro}>
+              <td>{order.tilausnro}</td>
+              <td>{order.asiakastiedot.etunimi} {order.asiakastiedot.sukunimi} (asnro: #{order.asiakas_id})</td>
+              <td>{order.tilausaika}</td>
+              <td className="text-end">
+                <ul className="list-unstyled">
+                {order.tuotteet.map(tuote => (
+                <li key={tuote.tuotenro}>{tuote.tuotenimi} x {tuote.kpl}</li>
+              ))}
+              </ul>
+              </td>
+            </tr>
+          ))}
+          {/* Tilausten mappaus päättyy */}
+        </tbody>
+      </table>
+    </>
+  )
+}
