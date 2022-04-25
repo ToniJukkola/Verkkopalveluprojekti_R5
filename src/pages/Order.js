e8c7eb1d7f9d81375577dd20d81fff5a5feae215
TIINA PELIMANNI <tiina.pelimanni@gmail.com>
2022-04-25 16:15:40 +0300

    Postitoimipaikan haku postinumeron perusteella

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -14,7 +14,8 @@ export default function Order({ url, cart, removeFromCart, updateAmount, emptyCa
   const [city, setCity] = useState("");
   const [error, setError] = useState("");
   const [isFinished, setIsFinished] = useState(false);
-  const [orderID, setOrderID] = useState("")
+  const [isLoading, setIsLoading] = useState(false);
+  const [orderID, setOrderID] = useState("");
 
   let sum = 0;
 
@@ -101,72 +102,88 @@ export default function Order({ url, cart, removeFromCart, updateAmount, emptyCa
     return true;
   }
 
-  if (isFinished === false) {
+  function handleZip() {
+    axios.get(url + "order/zipcodes.php/" + zip)
+      .then((response) => {
+        console.log(response.data);
+        setCity(response.data["toimipaikka"])
+      }).catch(error => {
+        alert(error.response === undefined ? error : error.response.data.error);
+      })
+  }
+
+  if (isLoading) {
     return (
-      <main className="container">
-        <h3>Ostoskorin tuotteet</h3>
-        <table>
-          <tbody>
-            {cart.map((product, index) => {
-              sum += parseFloat(product.hinta * product.amount);
-              return (
-                <tr key={uuid()}>
-                  <td>{product.tuotenimi}</td>
-                  <td>{product.hinta} €</td>
-                  <td><input style={{ width: 100 + "px", padding: 5 + "px" }} ref={inputs[index]} type="number" min="1" value={product.amount} onChange={e => changeAmount(e, product)} ></input></td>
-                  <td><button className="btn btn-dark" onClick={() => removeFromCart(product)}>Poista</button></td>
-                </tr>
-              )
-            })}
-            <tr key={uuid} style={{ borderTop: 1 + "px solid" }}>
-              <td>Yhteensä</td>
-              <td>{sum.toFixed(2)} €</td>
-              <td colSpan={2}></td>
-            </tr>
-          </tbody>
-        </table>
-
-        <button className="mt-4 btn btn-outline-dark" onClick={emptyCart}>Tyhjennä ostoskori</button>
-
-        <form className="mt-5" onSubmit={sendOrder}>
-          <h3>Toimitustiedot</h3>
-          <div className="mb-3">
-            <label htmlFor="firstname" className="form-label">Etunimi</label>
-            <input type="text" name="firstname" className="form-control" onChange={e => setFirstname(e.target.value)} required />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="lastname" className="form-label">Sukunimi</label>
-            <input type="text" name="lastname" className="form-control" onChange={e => setLastname(e.target.value)} required />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="address" className="form-label">Sähköposti</label>
-            <input type="text" name="email" className="form-control" onChange={e => setEmail(e.target.value)} required />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="address" className="form-label">Katuosoite</label>
-            <input type="text" name="address" className="form-control" onChange={e => setAddress(e.target.value)} required />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="zip" className="form-label">Postinumero</label>
-            <input type="text" name="zip" className="form-control" onChange={e => setZip(e.target.value)} required />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="city" className="form-label">Postitoimipaikka</label>
-            <input type="text" name="city" className="form-control" onChange={e => setCity(e.target.value)} required />
-          </div>
-          <button type="submit" className="btn btn-dark">Lähetä tilaus</button>
-        </form>
-
-        {error === "" ? "" :
-          <div className="alert alert-danger mt-3">{error}</div>
-        }
-      </main>
+      <main className="container"><h2 className="text-center">Ladataan ostoskoria...</h2></main>
     )
   } else {
-    return (
-    <main className="container">
-      <OrderSummary url={url} order={orderID} />
-    </main>
-    )
+    if (isFinished === false) {
+      return (
+        <main className="container">
+          <h3>Ostoskorin tuotteet</h3>
+          <table>
+            <tbody>
+              {cart.map((product, index) => {
+                sum += parseFloat(product.hinta * product.amount);
+                return (
+                  <tr key={uuid()}>
+                    <td>{product.tuotenimi}</td>
+                    <td>{product.hinta} €</td>
+                    <td><input style={{ width: 100 + "px", padding: 5 + "px" }} ref={inputs[index]} type="number" min="1" value={product.amount} onChange={e => changeAmount(e, product)} ></input></td>
+                    <td><button className="btn btn-dark" onClick={() => removeFromCart(product)}>Poista</button></td>
+                  </tr>
+                )
+              })}
+              <tr key={uuid} style={{ borderTop: 1 + "px solid" }}>
+                <td>Yhteensä</td>
+                <td>{sum.toFixed(2)} €</td>
+                <td colSpan={2}></td>
+              </tr>
+            </tbody>
+          </table>
+
+          <button className="mt-4 btn btn-outline-dark" onClick={emptyCart}>Tyhjennä ostoskori</button>
+
+          <form className="mt-5" onSubmit={sendOrder}>
+            <h3>Toimitustiedot</h3>
+            <div className="mb-3">
+              <label htmlFor="firstname" className="form-label">Etunimi</label>
+              <input type="text" name="firstname" className="form-control" onChange={e => setFirstname(e.target.value)} required />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="lastname" className="form-label">Sukunimi</label>
+              <input type="text" name="lastname" className="form-control" onChange={e => setLastname(e.target.value)} required />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="address" className="form-label">Sähköposti</label>
+              <input type="text" name="email" className="form-control" onChange={e => setEmail(e.target.value)} required />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="address" className="form-label">Katuosoite</label>
+              <input type="text" name="address" className="form-control" onChange={e => setAddress(e.target.value)} required />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="zip" className="form-label">Postinumero</label>
+              <input type="text" name="zip"  className="form-control" onChange={e => setZip(e.target.value)} onBlur={handleZip} required />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="city" className="form-label">Postitoimipaikka</label>
+              <input type="text" name="city" value={city} className="form-control" onChange={e => setCity(e.target.value)} required disabled />
+            </div>
+            <button type="submit" className="btn btn-dark">Lähetä tilaus</button>
+          </form>
+
+          {error === "" ? "" :
+            <div className="alert alert-danger mt-3">{error}</div>
+          }
+        </main>
+      )
+    } else {
+      return (
+        <main className="container">
+          <OrderSummary url={url} order={orderID} />
+        </main>
+      )
+    }
   }
 }
