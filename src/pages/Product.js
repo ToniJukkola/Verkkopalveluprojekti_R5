4c9eb287d7e9c660b99910308492f41cd8158907
TIINA PELIMANNI <tiina.pelimanni@gmail.com>
2022-04-25 16:14:38 +0300

    Lisätty loading... tuotelistauksiin

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -4,46 +4,56 @@ import { useParams } from 'react-router-dom'
 
 export default function Product({ url, addToCart }) {
   const [product, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   let params = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     axios.get(url + "products/get_product.php/" + params.productID)
       .then((response) => {
         setProduct(response.data[0]);
+        setIsLoading(false);
       }).catch(error => {
         alert(error.response === undefined ? error : error.response.data.error);
       })
   }, [url, params])
 
-  return (
-    <main className="container">
-      <div className='grid_container'>
-        <div>
-          <h3 className='product_name'>{product.tuotenimi}</h3>
-          <p className='scientific_name'>{product.tieteellinen_nimi}</p>
-          <img src={"http://localhost/verkkopalveluprojekti_r5_backend/images/tuotenro_" + product.tuotenro + ".jpg"} alt={product.tuotenimi} className="img-thumbnail" />
-          <h4 className='price mt-3'>{product.hinta} €</h4>
-        </div>
-        <div className="text-center">
-          <button className="btn btn-accent" onClick={e => addToCart(product)}><i className="bi bi-bag-fill"></i> Lisää ostoskoriin</button>
-          <label htmlFor="amount" className='amount'>KPL</label>
-          <select name="amount" id="amount">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
+  if (isLoading) {
+    return (
+      <main className="container"><h3 className="display-3 text-center">Ladataan...</h3></main>
+    )
+  } else {
 
-          <div className='circle'>
-            <div>
-              <p className='product_desc'>{product.tuotekuvaus}</p>
-              <p className='product_care'>{product.ohje}</p>
+    return (
+      <main className="container">
+        <div className='grid_container'>
+          <div>
+            <h3 className='product_name'>{product.tuotenimi}</h3>
+            <p className='scientific_name'>{product.tieteellinen_nimi}</p>
+            <img src={"http://localhost/verkkopalveluprojekti_r5_backend/images/tuotenro_" + product.tuotenro + ".jpg"} alt={product.tuotenimi} className="img-thumbnail" />
+            <h4 className='price mt-3'>{product.hinta} €</h4>
+          </div>
+          <div className="text-center">
+            <button className="btn btn-accent" onClick={e => addToCart(product)}><i className="bi bi-bag-fill"></i> Lisää ostoskoriin</button>
+            <label htmlFor="amount" className='amount'>KPL</label>
+            <select name="amount" id="amount">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+
+            <div className='circle'>
+              <div>
+                <p className='product_desc'>{product.tuotekuvaus}</p>
+                <p className='product_care'>{product.ohje}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  )
+      </main>
+    )
+  }
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,44 +5,53 @@ import { Link, useParams } from 'react-router-dom'
 export default function Products({ url, addToCart }) {
   const [categoryName, setCategoryName] = useState("");
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   let params = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     axios.get(url + "products/get_products.php/" + params.categoryID)
       .then((response) => {
         setCategoryName(response.data.category);
         setProducts(response.data.products);
+        setIsLoading(false);
       }).catch(error => {
         alert(error.response === undefined ? error : error.response.data.error);
       })
-  }, [url, params])
+  }, [url, params.categoryID])
 
+  if (isLoading) {
+    return (
+      <main className="container"><h3 className="display-3 text-center">Ladataan... tuotteita</h3></main>
+    )
+  } else {
 
-  return (
+    return (
 
-    <main className="container">
-      <h1>{categoryName}</h1>
-      <div className="product-list-wrapper">
-        {/* TUOTEKORTTIEN MAPPAUS ALKAA */}
-        {products?.map(product => (
-          <div className="card" key={product.tuotenro}>
-            <img src={"http://localhost/verkkopalveluprojekti_r5_backend/images/tuotenro_" + product.tuotenro + ".jpg"} className="card-img-top" alt="" />
-            <div className="card-body">
-              <div>
-                <h5 className="card-title">{product.tuotenimi}</h5>
-                <h6 className="card-title">{product.hinta}</h6>
+      <main className="container">
+        <h1>{categoryName}</h1>
+        <div className="product-list-wrapper">
+          {/* TUOTEKORTTIEN MAPPAUS ALKAA */}
+          {products?.map(product => (
+            <div className="card" key={product.tuotenro}>
+              <img src={"http://localhost/verkkopalveluprojekti_r5_backend/images/tuotenro_" + product.tuotenro + ".jpg"} className="card-img-top" alt="" />
+              <div className="card-body">
+                <div>
+                  <h5 className="card-title">{product.tuotenimi}</h5>
+                  <h6 className="card-title">{product.hinta}</h6>
+                </div>
+                <p className="card-text">{product.tuotekuvaus}
+                  <Link to={"/tuotteet/" + product.trnro + "/tuote/" + product.tuotenro}>Lue lisää</Link>
+                </p>
+
+                <button className="btn btn-accent" onClick={e => addToCart(product)}><i className="bi bi-bag-fill"></i> Lisää ostoskoriin</button>
               </div>
-              <p className="card-text">{product.tuotekuvaus}
-                <Link to={"/tuotteet/" + product.trnro + "/tuote/" + product.tuotenro}>Lue lisää</Link>
-              </p>
-
-              <button className="btn btn-accent" onClick={e => addToCart(product)}><i className="bi bi-bag-fill"></i> Lisää ostoskoriin</button>
             </div>
-          </div>
-        ))}
-        {/* TUOTEKORTTIEN MAPPAUS PÄÄTTYY */}
-      </div>
-    </main>
-  )
+          ))}
+          {/* TUOTEKORTTIEN MAPPAUS PÄÄTTYY */}
+        </div>
+      </main>
+    )
+  }
 }
