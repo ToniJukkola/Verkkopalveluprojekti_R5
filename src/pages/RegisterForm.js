e2ccf8a0756e02071a9c714f006a57658373a7c1
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-04-26 18:33:07 +0300

    Asiakkaan tunnuksen rekisteröinti toimii alustavasti.

--- src/pages/RegisterForm.js
+++ src/pages/RegisterForm.js
@@ -2,25 +2,10 @@ import axios from 'axios';
 import React, { useState } from "react";
 
 export default function Register({url}) {
-  const [password, setPassword] = useState(-1);
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [zip, setZip] = useState("");
-  const [city, setCity] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
-    
-
-    function handleZip() {
-      axios.get(url + "order/zipcodes.php/" + zip)
-        .then((response) => {
-          console.log(response.data);
-          setCity(response.data["toimipaikka"])
-        }).catch(error => {
-          alert(error.response === undefined ? error : error.response.data.error);
-        })
-    }
     
     function registerUser(e) {
         e.preventDefault();
@@ -28,15 +13,10 @@ export default function Register({url}) {
         if(password !== passwordCheck) {
           return alert("Salasanat eivät täsmää!");
         } else {
-
           const json = JSON.stringify({
-            etunimi: firstname,
-            sukunimi: lastname,
+            ktunnus: username,
             sposti: email,
-            password: password,
-            osoite: address,
-            postinro: zip,
-            postitmp: city,
+            salasana: password
           });
           axios.post(url + "admin/add_user.php", json, {
             headers: {
@@ -56,38 +36,22 @@ export default function Register({url}) {
     return (
         <main className="container">
           <form className="mt-5" onSubmit={registerUser}>
-            <h3>Toimitustiedot</h3>
+            <h3>Rekisteröidy</h3>
             <div className="mb-3">
-              <label htmlFor="firstname" className="form-label">Etunimi</label>
-              <input type="text" name="firstname" className="form-control" onChange={e => setFirstname(e.target.value)} required />
+              <label htmlFor="ktunnus" className="form-label">Käyttäjätunnus</label>
+              <input type="text" id="ktunnus" className="form-control" onChange={e => setUsername(e.target.value)} required />
             </div>
             <div className="mb-3">
-              <label htmlFor="lastname" className="form-label">Sukunimi</label>
-              <input type="text" name="lastname" className="form-control" onChange={e => setLastname(e.target.value)} required />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="address" className="form-label">Sähköposti</label>
-              <input type="text" name="email" className="form-control" onChange={e => setEmail(e.target.value)} required />
+              <label htmlFor="sposti" className="form-label">Sähköposti</label>
+              <input type="text" id="sposti" className="form-control" onChange={e => setEmail(e.target.value)} required />
             </div>
             <div>
-            <label htmlFor="address" className="form-label">Salasana</label>
-              <input type="password" name="email" className="form-control" onChange={e => setPassword(e.target.value)} required />
+            <label htmlFor="salasana" className="form-label">Salasana</label>
+              <input type="password" id="salasana" className="form-control" onChange={e => setPassword(e.target.value)} required />
             </div>
             <div>
-            <label htmlFor="address" className="form-label">Salasana uudelleen</label>
-              <input type="password" name="email" className="form-control" onChange={e => setPasswordCheck(e.target.value)} required />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="address" className="form-label">Katuosoite</label>
-              <input type="text" name="address" className="form-control" onChange={e => setAddress(e.target.value)} required />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="zip" className="form-label">Postinumero</label>
-              <input type="text" name="zip"  className="form-control" onChange={e => setZip(e.target.value)} onBlur={handleZip} required />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="city" className="form-label">Postitoimipaikka</label>
-              <input type="text" name="city" value={city} className="form-control" onChange={e => setCity(e.target.value)} required disabled />
+            <label htmlFor="passwordCheck" className="form-label">Salasana uudelleen</label>
+              <input type="password" id='passwordCheck' className="form-control" onChange={e => setPasswordCheck(e.target.value)} required />
             </div>
             <button type="submit" className="btn btn-dark">Lähetä tilaus</button>
           </form>
