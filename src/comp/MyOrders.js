bfecd62d5c0fddaf108ebeac7ba8eb46db92a86e
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-05-02 19:59:09 +0300

    Profiilisivu ei enää räjähdä, jos tilaushistoria 0

--- src/comp/MyOrders.js
+++ src/comp/MyOrders.js
@@ -1,15 +1,17 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-export default function MyOrders({url, token}) {
+export default function MyOrders({ url, token }) {
   const [orders, setOrders] = useState([]);
-  
+  const [hasOrders, setHasOrders] = useState(false);
+
   // Haetaan tilaukset
   useEffect(() => {
     axios.get(url + "users/get_own-orders.php/" + token)
       .then((response) => {
-        if (response) {
+        if (typeof response.data == "object") {
           setOrders(response.data);
+          setHasOrders(true);
         }
       }).catch(error => {
         alert(error.response === undefined ? error : error.response.data.error);
@@ -18,30 +20,30 @@ export default function MyOrders({url, token}) {
 
   return (
     <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>Tilausnro</th>
-            <th>Tilattu</th>
-            <th>Tuotteet</th>
-            <th>Loppusumma</th>
-          </tr>
-        </thead>
-        <tbody>
-        {orders.map(order => (
-            <tr key={order.tilausnro}>
-              <td>{order.tilausnro}</td>
-              <td>{order.tilausaika}</td>
-              <td>
-                <ul className="list-unstyled">
+      <thead>
+        <tr>
+          <th>Tilausnro</th>
+          <th>Tilattu</th>
+          <th>Tuotteet</th>
+          <th>Loppusumma</th>
+        </tr>
+      </thead>
+      <tbody>
+        {hasOrders ? orders.map(order => (
+          <tr key={order.tilausnro}>
+            <td>{order.tilausnro}</td>
+            <td>{order.tilausaika}</td>
+            <td>
+              <ul className="list-unstyled">
                 {order.tuotteet.map(tuote => (
-                <li key={tuote.tuotenro}>{tuote.tuotenimi} x {tuote.kpl}</li>
-              ))}
+                  <li key={tuote.tuotenro}>{tuote.tuotenimi} x {tuote.kpl}</li>
+                ))}
               </ul>
-              </td>
-              <td>{order.loppusumma}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            </td>
+            <td>{order.loppusumma}</td>
+          </tr>
+        )) : null}
+      </tbody>
+    </table>
   )
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,4 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import MyOrders from '../comp/MyOrders'
 import MyInfo from '../comp/MyInfo'
 
